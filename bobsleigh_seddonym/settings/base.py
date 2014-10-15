a9c894c15f412d43335703ab85a7005ef8f14e56
David Seddon <david@seddonym.me>
2014-10-15 15:27:28 +0100

    Added middleware classes to default settings

--- bobsleigh_seddonym/settings/base.py
+++ bobsleigh_seddonym/settings/base.py
@@ -90,11 +90,7 @@ TEMPLATE_CONTEXT_PROCESSORS += (
     'django.core.context_processors.request',
 )
 
-# During tests, disable migrations and use syncdb instead
-SOUTH_TESTS_MIGRATE = False
-
 ROOT_URLCONF = 'urls'
-
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -104,4 +100,13 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
